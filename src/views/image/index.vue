e389b94a340d36a07b7b6b95cfe28810742cec8e
Your Name <you@example.com>
2020-02-14 10:32:35 +0800

    素材管理-列表渲染

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -1,9 +1,94 @@
 <template>
-<div class='container'></div>
+  <div class="container-image">
+    <el-card>
+      <div slot="header">
+        <my-bread>素材管理</my-bread>
+      </div>
+      <!-- 按钮 -->
+      <div class="btn-box">
+        <!-- collect 值为true收藏的图片  false全部的图片 -->
+        <!-- :label 指定值才是布尔类型  -->
+        <el-radio-group v-model="reqParams.collect" size="small">
+          <el-radio-button :label="false">全部</el-radio-button>
+          <el-radio-button :label="true">收藏</el-radio-button>
+        </el-radio-group>
+        <el-button style="float:right" type="success" size="small">添加素材</el-button>
+      </div>
+      <!-- 列表 -->
+      <div class="img-list">
+        <div class="img-item" v-for="item in 1images" :key="item">
+          <img :src="item.url" alt />
+          <div class="option">
+            <span class="el-icon-star-off"></span>
+            <span class="el-icon-delete"></span>
+          </div>
+        </div>
+      </div>
+      <!-- 分页 -->
+      <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+    </el-card>
+  </div>
 </template>
-
 <script>
-export default {}
+export default {
+  name: 'app-image',
+  data () {
+    return {
+      // 查询条件
+      reqParams: {
+        collect: false,
+        page: 1,
+        per_page: 10
+      },
+      images:[]
+    }
+  },
+  created(){
+   this.getImages()
+  },
+  methods:{
+    async getImages(){
+      const res= await this.$http.get('user/images',{params:this.reqParams})
+      this.images=res.data.results
+    }
+  }
+}
 </script>
 
-<style scoped lang='less'></style>
+
+<style scoped lang='less'>
+.img-list {
+  margin-bottom: 15px;
+  .img-item {
+    width: 180px;
+    height: 180px;
+    border: 1px dashed #ddd;
+    position: relative;
+    margin-top: 20px;
+    display: inline-block;
+    margin-right: 30px;
+    img {
+      width: 100%;
+      height: 100%;
+      display: block;
+    }
+    .option {
+      width: 100%;
+      height: 30px;
+      line-height: 30px;
+      background: rgba(0, 0, 0, 0.3);
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      text-align: center;
+      span {
+        color: #fff;
+        margin: 0 30px;
+      }
+      .red{
+        color: red;
+      }
+    }
+  }
+}
+</style>
